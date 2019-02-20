29dd1b38293bd6ca68240b93f104787e918278d3
Luke Parker <lukep@embercrypto.io>
2019-02-20 05:37:16 -0600

    Add Nim procs for Value.

--- mc_lmdb/Value.nim
+++ mc_lmdb/Value.nim
@@ -1,3 +1,19 @@
 type
+    #Import the C struct.
     CValue {.header: "lmdb.h", importc: "MDB_val".} = object
+        mv_size: cuint
+        mv_data: pointer
+
+    #Pointer.
     Value* = ptr CValue
+
+#Constructor.
+proc newValue*(valueArg: string): Value =
+    #Copy the argument.
+    var value: string = valueArg
+
+    #Allocate the Value.
+    result = cast[Value](alloc0(sizeof(CValue)))
+    #Set the size and data fielda.
+    result.mv_size = cuint(value.len)
+    result.mv_data = addr value[0]
