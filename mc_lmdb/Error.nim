29276eb418e7b6899ddf1698621bcbeeace63579
Luke Parker <lukep@embercrypto.io>
2019-02-20 03:31:24 -0600

    Create LMDBError.

--- mc_lmdb/Error.nim
+++ mc_lmdb/Error.nim
@@ -1,1 +1,9 @@
+#LMDB Exception.
+type LMDBError = object of Exception
+
+#C proc to convert an error code to a string.
 proc c_mdb_strerror(code: cint): cstring {.header: "lmdb.h", importc: "mdb_strerror".}
+
+#Nim converter to run an error code into an Exception.
+converter toError*(code: cint): ref LMDBError =
+    newException(LMDBError, $c_mdb_strerror(code))
