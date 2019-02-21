668e621a9f66f2f3cbe5e5127e3e16d3eaf41edb
Luke Parker <lukep@embercrypto.io>
2019-02-20 23:45:54 -0600

    Add Nim procs for Environment.

--- mc_lmdb/Environment.nim
+++ mc_lmdb/Environment.nim
@@ -1,8 +1,16 @@
+#Error lib.
+import Error
+
+#Mode object (there is no wrapper file).
 import objects/ModeObject
 
+#Environment object.
 import objects/EnvironmentObject
 export EnvironmentObject.Environment
+export EnvironmentObject.EnvironmentFlags
+export EnvironmentObject.or
 
+#C procs.
 {.push header: "lmdb.h".}
 proc c_mdb_env_create(
     env: ptr Environment
@@ -19,3 +27,28 @@ proc c_mdb_env_close(
     env: Environment
 ) {.importc: "mdb_env_close".}
 {.pop.}
+
+#Constructor.
+proc newEnvironment*(
+    path: string,
+    flags: uint = EnvironmentFlags.NoSubDir or EnvironmentFlags.NoTLS or EnvironmentFlags.NoReadAhead,
+    mode: uint = 600
+): Environment =
+    #Create the Environment.
+    var err: cint = c_mdb_env_create(addr result)
+    #Check the error code.
+    err.check()
+
+    #Open the Environment.
+    err = c_mdb_env_open(
+        result,
+        path,
+        cuint(flags),
+        Mode(mode)
+    )
+    #Check the error code.
+    err.check()
+
+#Close an Environment.
+proc close*(env: Environment) =
+    c_mdb_env_close(env)
